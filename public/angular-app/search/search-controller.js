bc4551ddb1380d10cbb268ead9547368110db5e0
Jim McGee <jmcgee86@gmail.com>
2018-02-20 01:27:37 +0000

    search form reaching search-controller but not pulling data

--- public/angular-app/search/search-controller.js
+++ public/angular-app/search/search-controller.js
@@ -1,24 +1,27 @@
 angular.module('meannasdaq').controller('SearchController', SearchController);
 
-function SearchController ($http, $route, $routeParams, $window, stockDataFactory){
+function SearchController ($scope, $http, $route, $routeParams, $window, stockDataFactory){
     var vm = this;
-    vm.search = function(){
-        var Symbol = vm.symbol;
-        console.log("searching for " + Symbol);
-        
-         $http.get('/api/stocks/', Symbol).then(function(result){
-        console.log(result);
-                    vm.stock = result.data;
-                    vm.error = '';
-                }).catch(function(error){
-                    console.log(error);
-                });
-        
-        
-        // stockDataFactory.stockDisplay(Symbol).then(function(response){
-        // vm.stock = response.data;
-        // console.log(response.data)
-        // })
     
-    }
+    $scope.search = function(){
+        console.log("searching for " + Symbol);
+        Symbol=vm.symbol;
+        
+        
+        stockDataFactory.stockDisplay(Symbol).then(function(response){
+        vm.stock = response.data;
+        console.log(response.data)
+        })
+    
+    
     };
+}
+    
+            //  $http.get('/api/stocks/', Symbol).then(function(result){
+        // console.log(result);
+        //             vm.stock = result.data;
+        //             vm.error = '';
+        //         }).catch(function(error){
+        //             console.log(error);
+        //         });
+        
